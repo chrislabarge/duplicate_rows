173a532237b701fac4a3487ba1e874ff2b8f86d3
chrislabarge <chrislabarge2@gmail.com>
2018-05-24 19:36:16 -0400

    add help flag & update README

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # (SpreadSheet) Row Duplicator
 
-This is a simple script I wrote to duplicate some rows for spreadsheets. I just needed a program to quickly make file for mocking purposesa.
+This is a simple script I wrote to duplicate some rows for spreadsheets. I just needed a program to quickly make file for mocking purposes.
 
 ## Usage
 
-`ruby duplicate_rows.rb ARG[0] ARG[1]`
-`ruby duplicate_rows.rb File1 File2`
-`ruby duplicate_rows.rb YOUR_SPREADSHEET.xlsx NEW_SPREADSHEET_FILE.xlsx`
+- `ruby duplicate_rows.rb File1 File2`
+- `ruby duplicate_rows.rb ARG[0] ARG[1]`
+- `ruby duplicate_rows.rb YOUR_SPREADSHEET.xlsx NEW_SPREADSHEET_FILE.xlsx`
 
 The program takes 2 arguments. The first argument is the file path of the initial speadsheet you want to duplicate.  The second argument is the file path of the resulting file that contains the duplicated data.
 
@@ -18,3 +18,15 @@ You can also use the flag format `--limit`
 
 `ruby duplicate_rows.rb --limit 1000 YOUR_SPREADSHEET.xlsx NEW_SPREADSHEET_FILE.xlsx`
 
+## TODO
+
+- Add specs
+
+- Refactor
+
+- Allow for 'xls'
+
+## Author
+ Website: [ChrisLaBarge.com](http://chrislabarge.com)
+
+ Github: [github.com/chrislabarge](https://github.com/chrislabarge)
--- duplicate_rows.rb
+++ duplicate_rows.rb
@@ -4,30 +4,41 @@ require 'optparse'
 options = {}
 
 OptionParser.new do |parser|
+  parser.banner = "Usage: duplicate_rows.rb [options] file1 file2"
+
   parser.on("-l", "--limit LIMIT", "Number of Rows to limit appendage") do |num|
     options[:num] = num.to_i
   end
+
+  parser.on('-h', '--help' , 'Display Help') do
+    puts parser
+  end
 end.parse!
 
-workbook = RubyXL::Parser.parse(ARGV[0])
-options[:num] ||= -1
-all_rows = workbook[0].sheet_data.rows
-row_count = all_rows.count
-rows = all_rows[0..(options[:num])]
+unless ARGV[0] && ARGV[1]
+  puts 'You must provide the spreadsheet file path, and the new spreadsheet file path'
+else
 
-rows.each_with_index do |row, index|
-  next if index == 0 || row.nil?
+  workbook = RubyXL::Parser.parse(ARGV[0])
+  options[:num] ||= -1
+  all_rows = workbook[0].sheet_data.rows
+  row_count = all_rows.count
+  rows = all_rows[0..(options[:num])]
 
-  data = row.cells.map do |cell|
-    cell ? cell.value.to_s : cell.to_s
+  rows.each_with_index do |row, index|
+    next if index == 0 || row.nil?
+
+    data = row.cells.map do |cell|
+      cell ? cell.value.to_s : cell.to_s
+    end
+
+    row_count = row_count += 1
+
+    data.each_with_index do |value, i|
+      workbook[0].add_cell(row_count, i, value)
+    end
   end
 
-  row_count = row_count += 1
-
-  data.each_with_index do |value, i|
-    workbook[0].add_cell(row_count, i, value)
-  end
+  workbook.write(ARGV[1])
 end
-
-workbook.write(ARGV[1])
 
